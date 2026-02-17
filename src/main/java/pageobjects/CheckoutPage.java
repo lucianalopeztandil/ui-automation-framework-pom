a0700f9cb517b36c245b1e2edbaaf4e89da5a01e
luciana lopez <lucianalopeztandil625@gmail.com>
2026-02-17 05:47:21 -0300

    new waits

--- src/main/java/pageobjects/CheckoutPage.java
+++ src/main/java/pageobjects/CheckoutPage.java
@@ -1,6 +1,6 @@
 package pageobjects;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -27,13 +27,22 @@ public class CheckoutPage extends AbstractComponent {
     public void selectCountry(String countryName){
         inputCountry.sendKeys(countryName);
 
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".ta-results")));
-        countries.stream()
-                .filter(country -> country.getText().equalsIgnoreCase("India"))
+        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".ta-results")));
+        wait.until(ExpectedConditions.visibilityOfAllElements(countries));
+
+        WebElement target = countries.stream()
+                .filter(s -> s.getText().equalsIgnoreCase("India"))
                 .findFirst()
-                .ifPresent(country -> {
-                    wait.until(ExpectedConditions.elementToBeClickable(country)).click();
-                });
+                .orElse(null);
+
+        if (target != null) {
+            try {
+                wait.until(ExpectedConditions.elementToBeClickable(target)).click();
+            } catch (Exception e) {
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("arguments[0].click();", target);
+            }
+        }
     }
 
     public SucessPage clickSubmit(){
